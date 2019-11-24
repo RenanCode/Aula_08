60a948a067e39eb7ae9a46634009b1e5629858a4
Renan Costa <renan.barbosa@aluno.ifsp.edu.br>
2019-11-24 18:50:28 -0300

    Initial commit

---
+++ Questao_03.js
@@ -0,0 +1,10 @@
+var nome_aluno = "João da Silva";
+var n1 = 7;
+var n2 = 8.5;
+var m1 = n1 * 0.6;
+var m2 = n2 * 0.4;
+var nt = m1 + m2;
+
+alert(nome_aluno + "     N1 = " + m1 + "     N2 = " + m2);
+
+alert(nome_aluno + " sua nota total é: " + nt);
